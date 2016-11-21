84f904a9c33de285d50d3279f794a251d007b892
beckleon <beckleon@hotmail.com>
2016-11-21 18:49:52 +0800

    Update createTable.sql
    字符

--- createTable.sql
+++ createTable.sql
@@ -1,4 +1,4 @@
-#Ω®±Ì”Ôæ‰
+#Âª∫Ë°®ËØ≠Âè•
 CREATE TABLE Test1 (id int(5) NOT NULL, content Blob NOT NULL, PRIMARY KEY (id));
 CREATE TABLE Test2 (id int(5) NOT NULL, content Blob NOT NULL, PRIMARY KEY (id));
 CREATE TABLE Test3 (id int(5) NOT NULL, content Blob NOT NULL, PRIMARY KEY (id))
